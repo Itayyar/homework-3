bbfbd2c223fa2375e38edd4e2cef6637a81f97ce
Itayyar <itayyar22@gmail.com>
2025-06-21 21:46:26 +0300

    Update homework3_trigger.sql

--- homework3_trigger.sql
+++ homework3_trigger.sql
@@ -1,6 +1,6 @@
 -- Homework #3 - Database Trigger
--- Student: [Your Name Here]
--- University ID: [Your University ID Here]
+-- Student: [ibrahim mashhour ismail]
+-- University ID: [202311475]
 
 -- Trigger to ensure minimum salary of 500 on insert into employees table
 CREATE OR REPLACE TRIGGER trg_min_salary
